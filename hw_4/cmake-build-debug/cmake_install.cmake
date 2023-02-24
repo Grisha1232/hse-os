27cb9b20feb5d0e97660cedb3aacb52449718ccd
foxleren <foxleren@gmail.com>
2023-02-25 01:41:04 +0300

    upd

--- hw_4/cmake-build-debug/cmake_install.cmake
+++ hw_4/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/foxleren/Документы/GitHub/HW_4
+# Install script for directory: /home/foxleren/Документы/GitHub/OS_HW_4
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/foxleren/Документы/GitHub/HW_4/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/foxleren/Документы/GitHub/OS_HW_4/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
